2cbe9ce01176587dd334ec6caf89e3ab275788a5
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-10-30 12:20:31 +0900

    model 변경

--- crimereport/crime/admin.py
+++ crimereport/crime/admin.py
@@ -4,14 +4,14 @@ from .models import Congressperson, City, District, Detail
 # Register your models here.
 @admin.register(Congressperson)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['name', 'constituency_city', 'constituency_district','constituency_detail','crimes', 'photo', 'elected_num']
+    list_display = ['name', 'city_name', 'district_name','detail_name','crimes', 'photo', 'elected_num']
 
 @admin.register(City)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['city']
+    list_display = ['name']
 @admin.register(District)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['district']
+    list_display = ['name']
 @admin.register(Detail)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['detail']
+    list_display = ['name']
--- crimereport/crime/models.py
+++ crimereport/crime/models.py
@@ -1,19 +1,30 @@
 from django.db import models
 
 class City(models.Model):
-    city = models.CharField(blank=False, null = False, max_length=50) #지역구 시
+    name = models.CharField(blank=False, null = False, max_length=50) #지역구 시
+
+    def __str__(self):
+        return self.name
 
 class District(models.Model):
-    district = models.CharField(blank=False, null = False, max_length=50) #지역구 구
+    name = models.CharField(blank=False, null = False, max_length=50) #지역구 구
+
+    
+    def __str__(self):
+        return self.name
 
 class Detail(models.Model):
-    detail = models.CharField(blank=False, null = False, max_length=50) #지역구 갑/을/병
+    name = models.CharField(blank=False, null = False, max_length=50) #지역구 갑/을/병
+
+    
+    def __str__(self):
+        return self.name
 
 class Congressperson(models.Model):
     name = models.CharField(max_length=50)
-    constituency_city = models.ForeignKey(City,on_delete=models.CASCADE, related_name="c_city") #지역구 시
-    constituency_district = models.ForeignKey(District,on_delete=models.CASCADE, related_name="c_district") #지역구 구
-    constituency_detail = models.ForeignKey(Detail,on_delete=models.CASCADE, related_name="c_detail") #지역구 갑/을/병
+    city_name = models.ForeignKey('crime.City',on_delete=models.CASCADE) #지역구 시
+    district_name = models.ForeignKey('crime.District',on_delete=models.CASCADE) #지역구 구
+    detail_name = models.ForeignKey('crime.Detail',on_delete=models.CASCADE) #지역구 갑/을/병
     crimes = models.TextField(blank=True)
     photo = models.ImageField(upload_to='img',blank=True)
     elected_num = models.IntegerField(default=0)
