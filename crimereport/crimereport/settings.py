9e59b5af05a6924751b2c5b9962110f4207b1e28
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-11-02 13:57:35 +0900

    static 연결

--- crimereport/crimereport/settings.py
+++ crimereport/crimereport/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'crime', 'static') # 현재 static file의 위치
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # static file을 어디로 모일 것인지
