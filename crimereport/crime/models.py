d988832b4c294b58a208a1bdafacf74afb0924f3
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-11-03 16:17:13 +0900

    RESTful하게 model 재정의

--- crimereport/crime/models.py
+++ crimereport/crime/models.py
@@ -1,30 +1,17 @@
 from django.db import models
 
-class City(models.Model):
-    name = models.CharField(blank=False, null = False, max_length=50) #지역구 시
-
-    def __str__(self):
-        return self.name
-
-class District(models.Model):
-    name = models.CharField(blank=False, null = False, max_length=50) #지역구 구
-
+class Region(models.Model):
+    city = models.CharField(blank=False, null = False, max_length=50) #지역구 시
+    district = models.CharField(blank=True, null = True, max_length=50) #지역구 구
+    detail = models.CharField(blank=True, null = True, max_length=50) #지역구 갑/을/병
+    
     
     def __str__(self):
-        return self.name
-
-class Detail(models.Model):
-    name = models.CharField(blank=False, null = False, max_length=50) #지역구 갑/을/병
-
-    
-    def __str__(self):
-        return self.name
+        return f'{self.city} | {self.district} | {self.detail}'
 
 class Congressperson(models.Model):
     name = models.CharField(max_length=50)
-    city_name = models.ForeignKey('crime.City',on_delete=models.CASCADE) #지역구 시
-    district_name = models.ForeignKey('crime.District',on_delete=models.CASCADE) #지역구 구
-    detail_name = models.ForeignKey('crime.Detail',on_delete=models.CASCADE) #지역구 갑/을/병
+    city= models.ForeignKey('crime.Region',on_delete=models.CASCADE) #지역구   
     crimes = models.TextField(blank=True)
     photo = models.ImageField(upload_to='img',blank=True)
     elected_num = models.IntegerField(default=0)
