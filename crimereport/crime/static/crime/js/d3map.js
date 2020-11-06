86feff8cc743f09eb7aeeb779fdc3c66db593d86
unknown <skekdnl12@gmail.com>
2020-11-06 16:04:09 +0900

    2020.11.06 repair d3

--- crimereport/crime/static/crime/js/d3map.js
+++ crimereport/crime/static/crime/js/d3map.js
@@ -3,13 +3,14 @@ var topo_key = {20: {"provinces": "skorea_provinces_geo", "precinct": "precincts
 
 var assembly_no = 20;
 
-var height = document.querySelector('.pagearea').clientHeight;
-var width = 960 * height / 720;
+var width = document.querySelector('.d3-div').clientWidth;
+
+var height = 720 * width / 960;
 var active = d3.select(null);
 var choice;
 var proj = d3.geo.mercator()
-    .center([130.0, 35.9])
-    .scale(6000 * height / 720)
+    .center([128.0, 35.9])
+    .scale(6000)
     .translate([width/2, height/2]);
 
 var path = d3.geo.path()
@@ -34,7 +35,6 @@ var gp = g.append("g");
 
 d3.json(maps_path[assembly_no]["provinces"], function(error, kor) {
     var provinces = topojson.feature(kor, kor.objects[topo_key[assembly_no]["provinces"]]);
-    
     var g_provinces = gp.selectAll('g')
         .data(provinces.features, function(d) { return d.properties.code; })
         .enter()
@@ -47,7 +47,7 @@ d3.json(maps_path[assembly_no]["provinces"], function(error, kor) {
         .on("click", clicked)
         .append("title")
         .text(function(d) { return d.properties.name; });
-
+        // .on("click", cityClick()) click event 만들어주기
     g_provinces.append("text")
         .attr("class", "province-label")
         .attr("id", function(d) { return "province-label " + d.properties.code; })
@@ -57,6 +57,7 @@ d3.json(maps_path[assembly_no]["provinces"], function(error, kor) {
 
     g_provinces.on("mouseover", function() {
         d3.select(this).select("path").classed("highlighted", true);
+        
     });
     g_provinces.on("mouseout", function() {
         d3.select(this).select("path").classed("highlighted", false);
@@ -91,7 +92,8 @@ d3.json(maps_path[assembly_no]["precinct"], function(error, kor) {
 
     g_precincts.on("mouseover", function() {
         d3.select(this).select("path").classed("highlighted", true);
-        
+        $(this).on('click', cityClick())
+
         var html_text = '<h3>'+d3.select(this).datum().properties.precinct_name+'</h3>';
         // You can add more
         choice = d3.sel
@@ -109,61 +111,78 @@ d3.json(maps_path[assembly_no]["precinct"], function(error, kor) {
         d3.select(this).select("path").classed("highlighted", false);
         d3.select("#info")
             .style("visibility", "hidden");
+        $(this).on('click','')
     })
 
 });
 
 function clicked(d) {
-if (active.node() === this) return reset();
-active.classed("active", false);
-active = d3.select(this).classed("active", true);
+    if (active.node() === this) return reset();
+    // this 가 클릭한 도시의 path 태그
+    console.log(this)
+    active.classed("active", false);
+    active = d3.select(this).classed("active", true);
 
-var bounds = path.bounds(d),
-        dx = bounds[1][0] - bounds[0][0],
-        dy = bounds[1][1] - bounds[0][1],
-        x = (bounds[0][0] + bounds[1][0]) / 2,
-        y = (bounds[0][1] + bounds[1][1]) / 2,
-        scale = .7 / Math.max(dx / width, dy / height),
-        translate = [width / 2 - scale * x, height / 2 - scale * y];
+    var bounds = path.bounds(d),
+            dx = bounds[1][0] - bounds[0][0],
+            dy = bounds[1][1] - bounds[0][1],
+            x = (bounds[0][0] + bounds[1][0]) / 2,
+            y = (bounds[0][1] + bounds[1][1]) / 2,
+            scale = .7 / Math.max(dx / width, dy / height),
+            translate = [width / 2 - scale * x, height / 2 - scale * y];
+    console.log('bounds')
+        
+    console.log(bounds)
+    g.transition()
+            .duration(750)
+            .style("stroke-width", 1.5 / scale + "px")
+            .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
 
-g.transition()
-        .duration(750)
-        .style("stroke-width", 1.5 / scale + "px")
-        .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
+    d3.selectAll(".province").classed("selected", false);
+    d3.selectAll(".province").classed("notselected", false);
+    d3.selectAll(".precinct").classed("selected", false);
+    d3.selectAll("text.precinct-label").style("visibility", "hidden");
 
-d3.selectAll(".province").classed("selected", false);
-d3.selectAll(".province").classed("notselected", false);
-d3.selectAll(".precinct").classed("selected", false);
-d3.selectAll("text.precinct-label").style("visibility", "hidden");
+    d3.selectAll(".province").style("stroke-width", 2 / scale + "px");
+    d3.selectAll(".precinct").style("stroke-width", 1 / scale + "px");
 
-d3.selectAll(".province").style("stroke-width", 2 / scale + "px");
-d3.selectAll(".precinct").style("stroke-width", 1 / scale + "px");
-
-d3.selectAll("text.province-label").style("font-size", 14 / scale + "px");
+    d3.selectAll("text.province-label").style("font-size", 14 / scale + "px");
 
 
-if( d3.select(this).classed("province") ) {
-    d3.selectAll(".province").classed("notselected", true);
-    d3.select(this).classed("notselected", false);
-    d3.select(this).classed("selected", true);
-    d3.selectAll("text.precinct-label")
-        .style("visibility", "visible")
-        .style("font-size", 10 / scale + "px");
-}
+    if( d3.select(this).classed("province") ) {
+        d3.selectAll(".province").classed("notselected", true);
+        d3.select(this).classed("notselected", false);
+        d3.select(this).classed("selected", true);
+        d3.selectAll("text.precinct-label")
+            .style("visibility", "visible")
+            .style("font-size", 10 / scale + "px");
+    }
 }
 
 function reset() {
-active.classed("active", false);
-active = d3.select(null);
+    active.classed("active", false);
+    active = d3.select(null);
 
-g.transition()
-        .duration(750)
-        .style("stroke-width", "1.5px")
-        .attr("transform", "");
+    g.transition()
+            .duration(750)
+            .style("stroke-width", "1.5px")
+            .attr("transform", "");
 
-d3.selectAll(".province").classed("selected", false);
-d3.selectAll(".province").classed("notselected", false);
-d3.selectAll(".precinct").classed("selected", false);
-d3.selectAll("text.precinct-label").style("visibility", "hidden");
-d3.selectAll("text.province-label").style("font-size", "10px");
+    d3.selectAll(".province").classed("selected", false);
+    d3.selectAll(".province").classed("notselected", false);
+    d3.selectAll(".precinct").classed("selected", false);
+    d3.selectAll("text.precinct-label").style("visibility", "hidden");
+    d3.selectAll("text.province-label").style("font-size", "10px");
 }
+
+// function cityClick(d) {
+//     var city = $('.highlighted').text()
+//     var sendData = new Object();
+
+//     sendData.city = city
+//     sendData.district = $('#info').text()
+
+//     var jsonData = JSON.stringify(sendData)
+
+//     console.log(jsonData)
+// }
