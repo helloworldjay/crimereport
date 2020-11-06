f5d682e84db12fe00b0c429b3f65ea9bf5169ed5
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-11-06 21:13:25 +0900

    views에 search함수 만들어 JSON으로 원하는 데이터를 내보냄

--- crimereport/crime/urls.py
+++ crimereport/crime/urls.py
@@ -8,5 +8,5 @@ from . import views
 urlpatterns = [
     # path('', include(router.urls)),
     path('', views.CongressListView.as_view(), name='congresslist'),
-    path('<str:district>/', views.CongressDetailView.as_view(), name='district'),
+    path('<str:district>/', views.search,name="search"),
 ]
--- crimereport/crime/views.py
+++ crimereport/crime/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponse
 from .models import Congressperson
 from .serializer import CongresspersonSerializer
 from rest_framework import viewsets, mixins, generics
 from django.views.generic import DetailView, ListView
-
+import json
 
 #CBV
 
@@ -25,22 +25,35 @@ class CongressListView(ListView):
     # def get_queryset(self, district):
     #     return Congressperson.objects.get(district=district) 
 
-class CongressDetailView(DetailView):
-    model = Congressperson
-    template_name = 'crime/index.html'
-    context_object_name = "congresslistdetail"  
+def search(request, district):
+   info = Congressperson.objects.get(district=district)
+   name = info.name
+   city = info.district
+   crimes = info.crimes
+#    photo = info.photo
+   elected_num = info.elected_num
+   party = info.party
+   context = {"name":name, "city":city, "crimes":crimes, "elected_num":elected_num,"party":party}
+   context_json = json.dumps(context)
+   return HttpResponse(context_json)
 
 
-#     # def get_queryset(self,district):
-#     #     return Congressperson.objects.get(district=district) 
-#     # def get_queryset(self, district, *args, **kwargs):
-#     #     return Congressperson.objects.get(district=district)
+# class CongressDetailView(DetailView):
+#     model = Congressperson
+#     template_name = 'crime/index.html'
+#     context_object_name = "congresslistdetail"  
 
-    def get_context_data(self, **kwargs):
-        context = super(CongressDetailView, self).get_context_data(**kwargs)
-        context['all_objects'] = context.objects.all()
 
-        return context
+# #     # def get_queryset(self,district):
+# #     #     return Congressperson.objects.get(district=district) 
+# #     # def get_queryset(self, district, *args, **kwargs):
+# #     #     return Congressperson.objects.get(district=district)
+
+#     def get_context_data(self, **kwargs):
+#         context = super(CongressDetailView, self).get_context_data(**kwargs)
+#         context['all_objects'] = context.objects.all()
+
+#         return context
 
 # class CongressDetailView(DetailView):
 #     model = Congressperson
