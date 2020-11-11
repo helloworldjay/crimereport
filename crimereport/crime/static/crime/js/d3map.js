e13dd12c42e7c84ec3840af6ab5874b82e9f284c
unknown <skekdnl12@gmail.com>
2020-11-11 17:41:43 +0900

    2020.11.11 section3 right-column 수정

--- crimereport/crime/static/crime/js/d3map.js
+++ crimereport/crime/static/crime/js/d3map.js
@@ -139,13 +139,13 @@ function clicked(d) {
             dataType: "json",
             success: function (response) {
                 const baseurl = 'static/crime/img/'
-                $('.con_name').html(response.name)
-                $('.con_district').html(response.district)
+                $('#con_name').html(response.name)
+                $('#con_district').html(response.district)
                 $('.con_photo').attr('src',baseurl + response.photo)
-                $('.con_crimes').html(response.crimes)
-                $('.con_penalty').html(response.penalty)
-                $('.con_elected_num').html(response.elected_num)
-                $('.con_party').html(response.party)
+                $('#con_crimes').html(response.crimes)
+                $('#con_penalty').html(response.penalty)
+                $('#con_elected_num').html(response.elected_num)
+                $('#con_party').html(response.party)
 
                 
                 $('section:nth-child(2)').attr('class', 'background down-scroll');
