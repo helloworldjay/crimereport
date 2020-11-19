4c8c2c1e1697c0c1f8a2b66832347f4d36489dea
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-11-19 17:06:43 +0900

    urls path import 변경

--- crimereport/accounts/urls.py
+++ crimereport/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView
-from importlib.resources import path
+from django.urls import path
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),    
+    path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name='login'),    
 ]
