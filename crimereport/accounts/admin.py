67ecff7732dae27cc3d29fa2c868da164288c36c
unknown <skekdnl12@gmail.com>
2020-11-19 22:10:44 +0900

    2020.11.19 merge yc

--- crimereport/accounts/admin.py
+++ crimereport/accounts/admin.py
@@ -1,15 +1,7 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Profile # Region
 
 # Register your models here.
 @admin.register(Profile)
 class UserAdmin(admin.ModelAdmin):
     list_display = []
-=======
-from .models import Profile
-# Register your models here.
-@admin.register(Profile)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user_id']
->>>>>>> 02e15822065e770fdae67f97f3289754f761b1d7
