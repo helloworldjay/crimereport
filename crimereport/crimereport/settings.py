4be90923c7fa3c0275776b6d7fa9e1643f49fce6
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-11-21 17:42:23 +0900

    mysql 연동

--- crimereport/crimereport/settings.py
+++ crimereport/crimereport/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import db_settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -89,12 +89,15 @@ WSGI_APPLICATION = 'crimereport.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = db_settings.DATABASES
+
 
 
 # Password validation
