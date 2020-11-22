fe923af0662ccb9a7d5a118ae612c5e31ffefe76
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-11-22 15:20:42 +0900

    login

--- crimereport/accounts/views.py
+++ crimereport/accounts/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import redirect, render
-from django.views.generic import TemplateView, UpdateView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-# from .models import Profile
-# from .forms import ProfileForm
 from django.contrib.auth import get_user_model, login as auth_login
 from django.contrib.auth.forms import UserCreationForm
-from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import auth, messages
 from .models import User
 from .forms import SignupForm
+from django.contrib.auth.views import LoginView
 
+
+login = LoginView.as_view(template_name="accounts/login_form.html")
 
 # class ProfileView(LoginRequiredMixin, TemplateView): # login_required
 #     template_name = 'accounts/profile.html'
@@ -56,29 +55,29 @@ def signup(request):
         form = SignupForm()
     return render(request, "accounts/signup_form.html",{'form':form,})
 
-# 로그인
-def login(request):
-    # login으로 POST 요청이 들어왔을 때, 로그인 절차를 밟는다.
-    if request.method == 'POST':
-        # login.html에서 넘어온 username과 password를 각 변수에 저장한다.
-        username = request.POST['username']
-        password = request.POST['password']
+# # 로그인
+# def login(request):
+#     # login으로 POST 요청이 들어왔을 때, 로그인 절차를 밟는다.
+#     if request.method == 'POST':
+#         # login.html에서 넘어온 username과 password를 각 변수에 저장한다.
+#         username = request.POST['username']
+#         password = request.POST['password']
     
-        # 해당 username과 password와 일치하는 user 객체를 가져온다.
-        user = auth.authenticate(request, username=username, password=password)
+#         # 해당 username과 password와 일치하는 user 객체를 가져온다.
+#         user = auth.authenticate(request, username=username, password=password)
         
-        # 해당 user 객체가 존재한다면
-        if user is not None:
-            # 로그인 한다
-            auth.login(request, user)
-            return redirect('/')
-        # 존재하지 않는다면
-        else:
-            # 딕셔너리에 에러메세지를 전달하고 다시 login.html 화면으로 돌아간다.
-            return render(request, 'accounts/login_form.html', {'error' : 'username or password is incorrect.'})
-    # login으로 GET 요청이 들어왔을때, 로그인 화면을 띄워준다.
-    else:
-        return render(request, 'accounts/login_form.html')
+#         # 해당 user 객체가 존재한다면
+#         if user is not None:
+#             # 로그인 한다
+#             auth.login(request, user)
+#             return redirect('/')
+#         # 존재하지 않는다면
+#         else:
+#             # 딕셔너리에 에러메세지를 전달하고 다시 login.html 화면으로 돌아간다.
+#             return render(request, 'accounts/login_form.html', {'error' : 'username or password is incorrect.'})
+#     # login으로 GET 요청이 들어왔을때, 로그인 화면을 띄워준다.
+#     else:
+#         return render(request, 'accounts/login_form.html')
 
 # 로그 아웃
 def logout(request):
