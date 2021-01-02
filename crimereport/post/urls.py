f3ed6608cd5707cb76f102ecbbdaa3c6b440d577
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2021-01-02 17:00:54 +0900

    refactor: reset post app

--- crimereport/post/urls.py
+++ crimereport/post/urls.py
@@ -1,12 +1,5 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from . import views
-from django.views.generic import TemplateView
+from django.urls import path
 
 urlpatterns = [
-    path('postlist/',views.postlist, name='postlist'),
-    path('postlist/<int:post_id>/',views.detailpost, name='detailpost'),
-    path('postlist/<int:post_id>/updatepost/', views.updatepost, name='updatepost'),
-    path('postlist/writepost/', views.writepost, name='writepost'),
-    path('postlist/<int:post_id>/inputcomment/', views.ajaxcreatecomment, name="inputcomment"),
+    path('postlist/', views.dummy, name='postlist'),
 ]
