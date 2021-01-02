f73976988c98cfbb1e271db460add9119bd13966
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2021-01-02 17:00:20 +0900

    refactor: reset post app setting

--- crimereport/post/serializer.py
+++
@@ -1,7 +0,0 @@
-from .models import Comment
-from rest_framework import serializers
-
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment #Congressperson 모델을 기반으로 serialize
-        fields = '__all__'
